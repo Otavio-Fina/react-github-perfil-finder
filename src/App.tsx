26f997ce12b60a94b4f2124b674723c0ecf959f4
Otavio Fina <otaviofina@gmail.com>
2023-03-25 13:22:04 -0300

    vr 1.0.6
    sem react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 
 import classes from './App.module.css'
 
+import Home from './routes/Home'
+
 function App() {
 
   return (
     <div className={classes.app}>
       <h1>Github Finder</h1>
-      <Outlet />
+      <Home />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,27 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-//  Pages
-import Home from './routes/Home'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />        
-      }
-    ]
-  }
-  
-],{ basename : "/react-github-perfil-finder" })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
